bd861b25f732a908795c360f6f3fedc666162adf
riccardo.fantinel <riccardo.fantinel@electrolux.com>
2024-08-22 16:18:17 +0200

    remove player

--- fn_lib.py
+++ fn_lib.py
@@ -16,3 +16,16 @@ def navigate_to(page):
     st.session_state['page'] = page
     st.rerun()
 
+def find_last_not_none(lst):
+    # Iterate over the list in reverse
+    for item in reversed(lst):
+        if item is not None:
+            return item
+    return None  # Return None if all elements are None
+
+def count_not_none(lst):
+    count = 0
+    for item in lst:
+        if item is not None:
+            count += 1
+    return count
--- new_player_page.py
+++ new_player_page.py
@@ -1,7 +1,13 @@
 import streamlit as st
 import json
 
+import numpy as np 
+import pandas as pd
+
 from player import Player
+
+from fn_lib import find_last_not_none, count_not_none
+
 
 class NewPlayerPage():
     def __init__(self) -> None:
@@ -53,6 +59,46 @@ class NewPlayerPage():
 
 
 
-        self.player_list = st.selectbox('players list:', list(st.session_state.players_dict))
+            
+            selected_name = st.selectbox('Select player to remove:', list(st.session_state.players_dict))
+            if st.button("Remove Player"):
+                if selected_name in list(st.session_state.players_dict):
+
+                    del st.session_state.players_dict[selected_name]
+                    del st.session_state.data[selected_name]
+
+                    # Salva il dizionario in un file JSON
+                    with open(st.session_state.players_dict_file, 'w') as file:
+                        json.dump(st.session_state.data, file, indent=4)
+
+                    st.success(f'{selected_name} removed!')
+
+
+                else:
+                    st.error('Name selected not in list.')
+                
+                st.rerun()
+
        
-       
+
+        data = {}
+        df_cols_name = ['Total games', 'Played games', 'Won', 'Lost', 'Best score', 'Score']
+        for name in st.session_state.players_dict:
+            try:
+                idx_last_played_game = find_last_not_none(st.session_state.players_dict[name].games)
+                N_played_game = int(count_not_none(st.session_state.players_dict[name].games))
+
+                games_lost = int(np.sum(1-np.array(st.session_state.players_dict[name].games)))
+                games_won = int(np.sum(np.array(st.session_state.players_dict[name].games)))
+                Tot_games = int(st.session_state.players_dict[name].n_game[-1])+1
+                last_played_game = st.session_state.players_dict[name].n_game[idx_last_played_game]
+                last_score = st.session_state.players_dict[name].mu
+                best_score = np.max(np.array(st.session_state.players_dict[name].ranking_mu))
+                data[name] = [Tot_games, N_played_game, games_won, games_lost, best_score, last_score]
+            except:
+                data[name] = [None, None, None, None, None, None]
+
+        df = pd.DataFrame(data).transpose()
+        df = df.set_axis(df_cols_name, axis=1)
+
+        table = st.table(df)
